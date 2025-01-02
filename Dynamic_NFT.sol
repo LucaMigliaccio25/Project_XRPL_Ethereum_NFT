52bfd4243cf0fe8e77fdc529be2cfc97b39889f0
LucaM <luca.migliaccio4@studenti.unina.it>
2025-01-02 22:30:24 +0100

    Inizializzazione smart contract con licenza e import da OpenZeppelin.

---
+++ Dynamic_NFT.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+// Import dei contratti di OpenZeppelin direttamente da github
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.0/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.0/contracts/access/Ownable.sol";
