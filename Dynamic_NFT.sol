1b88c8e3ad49489b6ea6060906b0b8c97ce79db3
LucaM <luca.migliaccio4@studenti.unina.it>
2025-01-02 22:34:07 +0100

    Creazione della struttura base del contratto.

--- Dynamic_NFT.sol
+++ Dynamic_NFT.sol
@@ -4,3 +4,9 @@ pragma solidity ^0.8.20;
 // Import dei contratti di OpenZeppelin direttamente da github
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.0/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.0/contracts/access/Ownable.sol";
+
+/// @title DynamicNFT
+/// @notice Smart Contract per creare un NFT dinamico che rappresenta un avatar (con metadati variabili)
+contract DynamicNFT is ERC721URIStorage, Ownable {
+    
+}
